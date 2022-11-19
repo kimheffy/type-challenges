4b7e5510d0c6b2298c8857f798565df095a2789d
kimheffy <kimjeffrey96@gmail.com>
2022-11-19 13:12:40 -0800

    Intro to the types.

--- questions/00004-easy-pick/template.ts
+++ questions/00004-easy-pick/template.ts
@@ -1,1 +1,7 @@
-type MyPick<T, K> = any
+// Original template 
+// type MyPick<T, K> = any;
+
+// My solution:
+type MyPick<Type, Keys> = {
+    [Property in keyof Type as Extract<Property, Keys>]: Type[Property]
+};
--- questions/00011-easy-tuple-to-object/template.ts
+++ questions/00011-easy-tuple-to-object/template.ts
@@ -1,1 +1,3 @@
-type TupleToObject<T extends readonly any[]> = any
+type TupleToObject<T extends readonly any[]> = {
+  [V in T[number]]: V
+}
