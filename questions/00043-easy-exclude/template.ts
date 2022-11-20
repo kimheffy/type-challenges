bbe6a94547179e17676ec079c1f0c017a71f1662
kimheffy <kimjeffrey96@gmail.com>
2022-11-19 17:52:11 -0800

    Near complete with easy

--- questions/00043-easy-exclude/template.ts
+++ questions/00043-easy-exclude/template.ts
@@ -1,1 +1,3 @@
-type MyExclude<T, U> = T extends string ? 'c' : any
+// To learn more about this: 
+// check out https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-8.html#distributive-conditional-types 
+type MyExclude<T, U> = T extends U ? never : T;
--- questions/00898-easy-includes/template.ts
+++ questions/00898-easy-includes/template.ts
@@ -1,1 +1,3 @@
-type Includes<T extends readonly any[], U> = any
+import type { Equal } from '@type-challenges/utils';
+
+type Includes<T extends readonly any[], U> = T extends [infer head, ...infer tail] ? Equal<head, U> extends true ? true : Includes<tail, U> : false 
